b3b79b449ae1b174815f3fbc325328c82bbeedb5
Lucas Costa <luchtml5@gmail.com>
2022-12-10 09:47:47 -0300

    tab-nav transformado em class

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-import ScrollSuave from "./modules/scroll-suave.js";
-import Accordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
-import initToolTip from "./modules/tooltip.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetchanimais.js";
-import initFetchBitcoin from "./modules/fech-bitcoin.js";
-import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initToolTip from './modules/tooltip.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetchanimais.js';
+import initFetchBitcoin from './modules/fech-bitcoin.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"');
 scrollSuave.init();
@@ -16,8 +16,13 @@ scrollSuave.init();
 const accordion = new Accordion("[data-anime='accordion'] dt");
 accordion.init();
 
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section',
+);
+tabNav.init();
+
 initModal();
-initTabNav();
 initToolTip();
 initDropdownMenu();
 initMenuMobile();
